b00e217ffe0531c0c43d6e48b63976cf662a3edd
siyapareek <93838945+siyapareek@users.noreply.github.com>
2023-11-07 22:59:28 +0530

    Add files via upload

---
+++ Lecture 11 Bitwise Operator/src/Bitwise_Operators.java
@@ -0,0 +1,17 @@
+public class Bitwise_Operators {
+    public static void main(String[] args) {
+        int p=9;
+        int q=10;
+
+        System.out.println(p | q); //11
+        System.out.println(p & q); //8
+        System.out.println(p ^ q); //3
+
+        System.out.println(p<<1); //18
+        System.out.println(p<<2); //36
+
+        System.out.println(q>>1); //5
+        System.out.println(q>>2); //2
+
+    }
+}
