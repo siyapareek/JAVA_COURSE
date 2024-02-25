babcf8ca45882edbcbf707a786ce388749f09ad7
siyapareek <93838945+siyapareek@users.noreply.github.com>
2024-02-25 20:01:41 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,8 @@
+public class Person {
+    int age=20;
+
+    public static void main(String[] args) {
+        Person rohan=new Person();
+        System.out.println(rohan.age);
+    }
+}
---
+++ student.java
@@ -0,0 +1,12 @@
+public class student {
+    int rollNo;
+    String student_name;
+
+    public static void main(String[] args) {
+        student obj1=new student();
+        System.out.println(obj1.rollNo);
+        System.out.println(obj1.student_name);
+
+
+    }
+}
